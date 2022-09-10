0ff9f240e5183563b5f93b8a81b6e128ba5e648f
Igor Lugovskoy <86158988+GoshaOlegovich@users.noreply.github.com>
2022-09-10 21:55:10 +0300

    Update UI

--- build/script/script.js
+++ build/script/script.js
@@ -1,148 +1,160 @@
+// Global
+
+let currentСurrency = "$";
 
 // Mock BACKEND
 
 const transactions = [
   {
     type: "income",
-    value: 20000,
+    value: 500,
+    category: "Freelance",
     date: {
       month: "Jule",
       day: 01,
     },
   },
   {
+    type: "outcome",
+    value: 100,
+    category: "Food",
+    date: {
+      month: "Aug",
+      day: 8,
+    },
+  },
+  {
     type: "income",
-    value: 40000,
+    value: 500,
+    category: "Freelance",
     date: {
       month: "Jule",
-      day: 10,
+      day: 01,
     },
   },
   {
     type: "outcome",
-    value: 1300,
+    value: 90,
+    category: "Food",
+    date: {
+      month: "Sep",
+      day: 8,
+    },
+  },
+  {
+    type: "income",
+    value: 490,
+    category: "Freelance",
+    date: {
+      month: "Jule",
+      day: 01,
+    },
+  },
+  {
+    type: "outcome",
+    value: 30,
+    category: "Food",
     date: {
       month: "Jule",
       day: 8,
     },
   },
-  {
-    type: "outcome",
-    value: 2000,
-    date: {
-      month: "Jule",
-      day: 30,
-    },
-  },
-  {
-    type: "outcome",
-    value: 2000,
-    date: {
-      month: "Jule",
-      day: 01,
-    },
-  },
-  {
-    type: "outcome",
-    value: 2000,
-    date: {
-      month: "Jule",
-      day: 10,
-    },
-  },
-  {
-    type: "income",
-    value: 20000,
-    date: {
-      month: "Jule",
-      day: 01,
-    },
-  },
-  {
-    type: "income",
-    value: 3000,
-    date: {
-      month: "Jule",
-      day: 10,
-    },
-  },
-  {
-    type: "income",
-    value: 3000,
-    date: {
-      month: "Jule",
-      day: 10,
-    },
-  },
-  {
-    type: "income",
-    value: 3000,
-    date: {
-      month: "Jule",
-      day: 10,
-    },
-  },
-  {
-    type: "income",
-    value: 123,
-    date: {
-      month: "Jule",
-      day: 10,
-    },
-  },
 ];
 
-//
+// => Form
 
-const list = document.querySelector(".dashboard__actives_list");
+const form = document.querySelector(".transactions__form");
 
-const includeTransactions = () => {
-  for (let i = 0; i < transactions.length; i++) {
+form.addEventListener("change", (e) => {
+  let chage = e.target.value;
+
+  let month = document.getElementById("selectorMonth"),
+    category = document.getElementById("selectorCategory"),
+    type = document.getElementById("selectorType");
+
+  const filterParam = {
+    month: month.value,
+    category: category.value,
+    type: type.value,
+  };
+
+  console.log(filterParam);
+
+  list.innerHTML = `
+  <li class="transactions_item">
+                  <span class="transactions__value">
+                    Category
+                  </span>
+                  <span class="transactions__value">
+                    Date
+                  </span>
+                  <span class="transactions__value">
+                    Amount
+                  </span>
+                </li>`;
+
+  includeTransactions(
+    filter(filterParam.month, filterParam.category, filterParam.type)
+  );
+
+});
+
+const filter = (param1, param2, param3) => {
+
+};
+
+// => Chart
+
+const list = document.querySelector(".transactions__list");
+
+const includeTransactions = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
     const item = document.createElement("li");
-    item.className = "dashboard__actives_item";
+    item.className = "transactions_item";
 
     const type = document.createElement("span");
-    type.className = "dashboard__actives-value dashboard__actives-value-type";
+    type.className = "transactions__value transactions__value--type";
 
     const value = document.createElement("span");
-    value.className =
-      "dashboard__actives-value dashboard__actives-value-amount";
+    value.className = "transactions__value transactions__value--amount";
 
     const date = document.createElement("div"),
+      category = document.createElement("span"),
       month = document.createElement("span"),
       day = document.createElement("span");
-    month.className = "dashboard__actives-value";
-    day.className = "dashboard__actives-value";
+    category.className = "transactions__value";
+    month.className = "transactions__value";
+    day.className = "transactions__value";
 
-    month.innerHTML = transactions[i].date.month;
-    day.innerHTML = transactions[i].date.day;
+    category.innerHTML = arr[i].category;
+    month.innerHTML = arr[i].date.month;
+    day.innerHTML = arr[i].date.day;
 
     date.appendChild(month);
 
-    type.innerHTML = typeCheker(transactions[i].type);
-    value.innerHTML = `$ ${transactions[i].value}`;
+    value.innerHTML = typeCheker(arr[i].value, arr[i].type);
 
-
-    item.appendChild(value);
+    item.appendChild(category);
     item.appendChild(date);
-    item.appendChild(type);
+    item.appendChild(value);
+
     list.appendChild(item);
   }
 };
 
-const typeCheker = (x) => {
-  if (x == "income") {
-    return "<div class='marker marker-income'></div>";
+const typeCheker = (amount, type) => {
+  if (type == "income") {
+    return `+ ${currentСurrency} ${amount}`;
   } else {
-    return "<div class='marker marker-outcome'></div>";;
+    return `- ${currentСurrency} ${amount}`;
   }
 };
 
 //
 
-const earning = document.querySelector(".dashboard__info_amount__earning"),
-  speding = document.querySelector(".dashboard__info_amount__spending"),
-  balance = document.querySelector(".dashboard__info_amount__balance");
+const earning = document.querySelector(".analytic__info_amount__earning"),
+  speding = document.querySelector(".analytic__info_amount__spending"),
+  balance = document.querySelector(".analytic__info_amount__balance");
 
 const income = transactions.filter((el) => el.type === "income"),
   incomeTotal = income.reduce((a, b) => a + b.value, 0);
@@ -158,7 +170,9 @@ balance.innerHTML = `$ ${totalBalance}`;
 const incomeValueArr = income.map((i) => i.value);
 const outcomeValueArr = outcome.map((i) => i.value);
 
-includeTransactions();
+includeTransactions(transactions);
+
+// => Chart
 
 const labels = ["Jule"];
 
@@ -170,17 +184,14 @@ const data = {
       backgroundColor: "green",
       borderColor: "green",
       data: incomeValueArr,
-      
     },
     {
       label: "Outcome",
       backgroundColor: "red",
       borderColor: "red",
       data: outcomeValueArr,
-      
     },
   ],
-
 };
 
 const config = {
